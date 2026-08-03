ba6b30ca2948a01dd20858eb622bbdb2111c1432
Fernando Biaggi <FBinnoApp@proton.me>
2026-08-03 10:50:58 +0200

    chore: remove unused prototype packages

--- packages/intelligence/src/service/IntelligenceServices.ts
+++
@@ -1,9 +0,0 @@
-export interface IntelligenceService {
-  summarize(text: string): Promise<string>;
-
-  categorize(text: string): Promise<string[]>;
-
-  findConnections(id: string): Promise<string[]>;
-
-  suggestCollections(): Promise<string[]>;
-}
